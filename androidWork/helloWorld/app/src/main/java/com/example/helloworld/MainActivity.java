a3a4352ef86810ed0881d84f06cde40b0b09d1bc
leeyj341 <leeyj3794@gmail.com>
2020-03-24 16:44:50 +0900

    Android 기본 구조 연습

---
+++ androidWork/helloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+//Activity의 LifeCycle
+public class MainActivity extends AppCompatActivity {
+    //Activity가 생성될 때 자동으로 호출
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //System.out.println("onCreate호출~~~");
+        Log.d("test", "onCreate호출");
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("test", "onStart()호출");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("test", "onResume()호출");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("test", "onPause()호출");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("test", "onDestroy()호출");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("test", "onStop()호출");
+    }
+
+
+
+    // 버튼을 클릭했을 때 실행할 메서드를 정의
+    // 메서드의 매개변수에 실행할 버튼을 정의
+    // Button의 상위인 view타입으로 정의
+    public void myclickMethod(View v) {
+        //                                    메시지                 메시지 띄울 시간
+        Toast.makeText(this, "확인버튼이 눌렸습니다.", Toast.LENGTH_LONG).show();
+    }
+
+    public void mycancelMethod(View v) {
+        Toast.makeText(this,"취소버튼이 눌렸습니다.",Toast.LENGTH_SHORT).show();
+    }
+
+    public void mydeleteMethod(View v) {
+        Toast.makeText(this,"삭제버튼이 눌렸습니다.",Toast.LENGTH_LONG).show();
+    }
+}
